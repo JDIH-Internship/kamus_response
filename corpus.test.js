5c8b5a1c4789a896ba1240addabccc63ed7a49bc
sandi hermawan <116500877+sandi-hermawan01@users.noreply.github.com>
2023-05-27 19:40:16 +0700

    update-corpus-test

---
+++ corpus.test.js
@@ -0,0 +1,62 @@
+module.exports = [
+  {
+    input: "Assalamu'alaikum",
+    expected: "agen.sapaan-islam",
+  },
+  {
+    input: "halo selamat pagi",
+    expected: "agen.sapaan-pagi",
+  },
+  {
+    input: "halo min selamat siang",
+    expected: "agen.sapaan-siang",
+  },
+  {
+    input: "sore min saya mau tanya",
+    expected: "agen.sapaan-sore",
+  },
+  {
+    input: "malam saya ingin bertanya",
+    expected: "agen.sapaan-malam",
+  },
+  {
+    input: "cara membuat sertifikat tanah",
+    expected: "agen.setifikat-tanah",
+  },
+  {
+    input: "bantuan",
+    expected: "agen.format-penulisan",
+  },
+  {
+    input: "Bisa jelaskan mengenai apa itu jdih",
+    expected: "agen.apa-itu-jdih",
+  },
+  {
+    input: "aku pengen tau dong tentang visi jdih",
+    expected: "agen.visi-jdih",
+  },
+  {
+    input: "Apakah Anda bisa menjelaskan apa yang dimaksud dengan misi jdih",
+    expected: "agen.misi-jdih",
+  },
+  {
+    input: "bisa jelaskan mengenai tugas pokok jdih",
+    expected: "agen.tugas-jdih",
+  },
+  {
+    input: "saya ingin tau mengenai tujuan jdih",
+    expected: "agen.tujuan-jdih",
+  },
+  {
+    input: "apa itu fungsi jdih",
+    expected: "agen.fungsi-jdih",
+  },
+  {
+    input: "Terima kasih atas bantuannya",
+    expected: "agen.terimakasih",
+  },
+  {
+    input: "Terima kasih",
+    expected: "agen.terimakasih",
+  },
+];
