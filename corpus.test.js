fb293cb56d8cdab3791ba0d760f49347c4653b15
sandi hermawan <116500877+sandi-hermawan01@users.noreply.github.com>
2023-05-28 11:52:33 +0700

    Update kategori TP, TN, TF, dll.

--- corpus.test.js
+++ corpus.test.js
@@ -2,61 +2,76 @@ module.exports = [
   {
     input: "Assalamu'alaikum",
     expected: "agen.sapaan-islam",
+    kategori: "TP",
   },
   {
     input: "halo selamat pagi",
     expected: "agen.sapaan-pagi",
+    kategori: "TP",
   },
   {
     input: "halo min selamat siang",
     expected: "agen.sapaan-siang",
+    kategori: "TP",
   },
   {
     input: "sore min saya mau tanya",
     expected: "agen.sapaan-sore",
+    kategori: "TP",
   },
   {
     input: "malam saya ingin bertanya",
     expected: "agen.sapaan-malam",
+    kategori: "TP",
   },
   {
     input: "cara membuat sertifikat tanah",
     expected: "agen.setifikat-tanah",
+    kategori: "TN",
   },
   {
     input: "bantuan",
     expected: "agen.format-penulisan",
+    kategori: "TP",
   },
   {
     input: "Bisa jelaskan mengenai apa itu jdih",
     expected: "agen.apa-itu-jdih",
+    kategori: "TP",
   },
   {
     input: "aku pengen tau dong tentang visi jdih",
     expected: "agen.visi-jdih",
+    kategori: "TP",
   },
   {
     input: "Apakah Anda bisa menjelaskan apa yang dimaksud dengan misi jdih",
     expected: "agen.misi-jdih",
+    kategori: "TP",
   },
   {
     input: "bisa jelaskan mengenai tugas pokok jdih",
     expected: "agen.tugas-jdih",
+    kategori: "TP",
   },
   {
     input: "saya ingin tau mengenai tujuan jdih",
     expected: "agen.tujuan-jdih",
+    kategori: "TP",
   },
   {
     input: "apa itu fungsi jdih",
     expected: "agen.fungsi-jdih",
+    kategori: "TP",
   },
   {
     input: "Terima kasih atas bantuannya",
     expected: "agen.terimakasih",
+    kategori: "TP",
   },
   {
     input: "Terima kasih",
     expected: "agen.terimakasih",
+    kategori: "TP",
   },
 ];
